993c3b2ca42b86bf15871f472e9023868cd0f605
MahdiRazzaque <mahdi@razzaque.co.uk>
2022-01-06 19:23:58 +0000

    Cleaned up initialTicket.js

--- events/ticket/initialTicket.js
+++ events/ticket/initialTicket.js
@@ -17,7 +17,7 @@ module.exports = {
         
         if(!ticket_enabled) return interaction.reply({embeds: [new MessageEmbed().setColor("RED").setDescription("Tickets are currently disabled.")], ephemeral: true})
         
-        if (customId === "dungeon-ticket") return interaction.reply({embeds: [new MessageEmbed().setDescription("Dungeon carries are not currently available").setColor("RED")], ephemeral: true})
+        //if (customId === "dungeon-ticket") return interaction.reply({embeds: [new MessageEmbed().setDescription("Dungeon carries are not currently available").setColor("RED")], ephemeral: true})
         //if (customId === "slayer-ticket") return interaction.reply({embeds: [new MessageEmbed().setDescription("Slayer carries are not currently available").setColor("RED")], ephemeral: true})
 
         let ticketChannel = (interaction.guild.channels.cache.find(c => c.name.toLowerCase().includes(member.id)))
@@ -44,93 +44,62 @@ module.exports = {
                 .setEmoji("🔓")
         )
 
+        const Embed = new MessageEmbed()
+
         switch (customId) {
             case "slayer-ticket":
-                await guild.channels.create(`${customId + "-" + ID + "-" + interaction.user.id}`, {
-                    type: "GUILD_TEXT",
-                    parent: parent_id,
-                    permissionOverwrites: [
-                        {
-                            id: "917054077869912177",
-                            allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                        {
-                            id: member.id,
-                            allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                        {
-                            id: everyone_id,
-                            deny: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                    ],
-                })
-                .then(async(channel) => {
-                    await DB.create({ GuildID: guild.id, MemberID: member.id, TicketID: ID, ChannelID: channel.id, Closed: false, Locked: false, Type: customId })
+                var helperRoleId = "917054077869912177"
 
-                    const Embed = new MessageEmbed()
-
-                    Embed.setAuthor(`${guild.name} | Ticket: ${ID}`, guild.iconURL({dynamic:true}))
-                    Embed.setTitle(`Hello and welcome to your slayer carrying service.`)
-                    Embed.setDescription(`**A <@&917054077869912177> will be with you shortly.** \n \n While you wait please follow the following for requesting your carry: \n> IGN: \n> Slayer which you want killed: \n> Number of bosses: \n> If you need to borrow goblin armour and wither cloak sword:`)
-                    Embed.addFields(
-                        {name: "💰 Prices", value: "<#917056569353580584>", inline: true},
-                        {name: "🏕 How to survive", value: "<#917063326587031603>", inline: true},
-                        {name: "❔ Frequent Questions", value: "<#917073516677967903>", inline: true}
-                    )
-                    Embed.setFooter("The buttons below are staff only buttons.")
-                    Embed.setColor(open_ticket_embed_colour)
-                    Embed.setTimestamp()
-
-                    channel.send({content: `Welcome <@${interaction.user.id}> ||<@&917054077869912177>||`, embeds: [Embed], components: [Buttons]})
-
-                    await channel.send({content: `${member} here is your ticket.`}).then((m) => {
-                        setTimeout(() => {
-                            m.delete().catch(() => {})
-                        }, 1 * 10000)
-                    })
-        
-                    interaction.reply({embeds: [new MessageEmbed().setDescription(`${member} your ticket has been created: ${channel}`).setColor("GREEN")], ephemeral: true})
-                })
+                Embed.setAuthor(`${guild.name} | Ticket: ${ID}`, guild.iconURL({dynamic:true}))
+                Embed.setTitle(`Hello and welcome to your slayer carrying service.`)
+                Embed.setDescription(`**A <@&917054077869912177> will be with you shortly.** \n \n While you wait please follow the following for requesting your carry: \n> IGN: \n> Slayer which you want killed: \n> Number of bosses: \n> If you need to borrow goblin armour and wither cloak sword:`)
+                Embed.addFields(
+                    {name: "💰 Prices", value: "<#917056569353580584>", inline: true},
+                    {name: "🏕 How to survive", value: "<#917063326587031603>", inline: true},
+                    {name: "❔ Frequent Questions", value: "<#917073516677967903>", inline: true}
+                )
+                Embed.setFooter("The buttons below are staff only buttons.")
+                Embed.setColor(open_ticket_embed_colour)
+                Embed.setTimestamp()
                 break;
             case "dungeon-ticket":
-                await guild.channels.create(`${customId + "-" + ID + "-" + interaction.user.id}`, {
-                    type: "GUILD_TEXT",
-                    parent: parent_id,
-                    permissionOverwrites: [
-                        {
-                            id: "925013507546693632",
-                            allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                        {
-                            id: member.id,
-                            allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                        {
-                            id: everyone_id,
-                            deny: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
-                        },
-                    ],
-                })
-                .then(async(channel) => {
-                    await DB.create({ GuildID: guild.id, MemberID: member.id, TicketID: ID, ChannelID: channel.id, Closed: false, Locked: false, Type: customId })
+                var helperRoleId = "925013507546693632"
+                Embed.setAuthor(`${guild.name} | Ticket: ${ID}`, guild.iconURL({dynamic:true}))
+                Embed.setDescription("This is a dungeon ticket (WIP)!")
+                Embed.setFooter("The buttons below are staff only buttons.")
+                Embed.setColor(open_ticket_embed_colour)
 
-                    const Embed = new MessageEmbed()
-
-                    Embed.setAuthor(`${guild.name} | Ticket: ${ID}`, guild.iconURL({dynamic:true}))
-                    Embed.setDescription("This is a dungeon ticket (WIP)!")
-                    Embed.setFooter("The buttons below are staff only buttons.")
-                    Embed.setColor(open_ticket_embed_colour)
-
-                    channel.send({content: `Welcome <@${interaction.user.id}> ||<@&925013507546693632>||`, embeds: [Embed], components: [Buttons]})
-
-                    await channel.send({content: `${member} here is your ticket.`}).then((m) => {
-                        setTimeout(() => {
-                            m.delete().catch(() => {})
-                        }, 1 * 10000)
-                    })
-        
-                    interaction.reply({embeds: [new MessageEmbed().setDescription(`${member} your ticket has been created: ${channel}`).setColor("GREEN")], ephemeral: true})
-                })
         }
+        await guild.channels.create(`${customId + "-" + ID + "-" + interaction.user.id}`, {
+            type: "GUILD_TEXT",
+            parent: parent_id,
+            permissionOverwrites: [
+                {
+                    id: helperRoleId,
+                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                },
+                {
+                    id: member.id,
+                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                },
+                {
+                    id: everyone_id,
+                    deny: ["SEND_MESSAGES", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY"]
+                },
+            ],
+        })
+        .then(async(channel) => {
+            await DB.create({ GuildID: guild.id, MemberID: member.id, TicketID: ID, ChannelID: channel.id, Closed: false, Locked: false, Type: customId })
+
+            channel.send({content: `Welcome <@${interaction.user.id}> ||<@&${helperRoleId}>||`, embeds: [Embed], components: [Buttons]})
+
+            await channel.send({content: `${member} here is your ticket.`}).then((m) => {
+                setTimeout(() => {
+                    m.delete().catch(() => {})
+                }, 1 * 10000)
+            })
+
+            interaction.reply({embeds: [new MessageEmbed().setDescription(`${member} your ticket has been created: ${channel}`).setColor("GREEN")], ephemeral: true})
+        })
     }
 }
