5ba69787a0512f0d32929b66d5c663a8389616a6
MahdiRazzaque <mahdi@razzaque.co.uk>
2022-04-07 16:51:19 +0100

    Update antiCrash.js

--- structures/antiCrash.js
+++ structures/antiCrash.js
@@ -1,8 +1,7 @@
 const chalk = require("chalk");
-const config = require('./config.json')
-const {MessageEmbed, WebhookClient} = require('discord.js')
-const {inspect} = require("util")
-const s = new WebhookClient({url: "https://discord.com/api/webhooks/961270146222153809/iR6wk6DW-St8eOlGv0nXN4h9ge2fVf8em5ComG0mwggFQLANRA3oBUb-jEwT5GD4FnX_"})
+const { error_logs_id } = require('./config.json')
+const { MessageEmbed } = require('discord.js')
+const { inspect } = require("util")
 
 
 module.exports = (client) => {
@@ -12,26 +11,22 @@ module.exports = (client) => {
             .setTitle('Error')
             .setColor('#2F3136')
             .setDescription(`\`\`\`${inspect(error, {depth: 0})}\`\`\``)
-            
             .setTimestamp()
-        return s.send({
-            embeds: [ErrorEmbed]
-        })
+        
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [ErrorEmbed] })
     });
     process.on("unhandledRejection", (reason, p) => {
-        console.log(
-            chalk.yellow('——————————[Unhandled Rejection/Catch]——————————\n'),
-            reason, p
-        )
+        console.log(chalk.yellow('——————————[Unhandled Rejection/Catch]——————————\n'), reason, p)
         const unhandledRejectionEmbed = new MessageEmbed()
             .setTitle('Unhandled Rejection/Catch')
             .setColor('RED')
             .addField('Reason', `\`\`\`${inspect(reason, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .addField('Promise', `\`\`\`${inspect(p, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .setTimestamp()
-        return s.send({
-            embeds: [unhandledRejectionEmbed]
-        })
+        
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [unhandledRejectionEmbed] })
     });
     
     process.on("uncaughtException", (err, origin) => {
@@ -42,24 +37,23 @@ module.exports = (client) => {
             .addField('Error', `\`\`\`${inspect(err, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .addField('Origin', `\`\`\`${inspect(origin, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .setTimestamp()
-        return s.send({
-            embeds: [uncaughtExceptionEmbed]
-        })
+
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [uncaughtExceptionEmbed] })
     });
     
-    // process.on("uncaughtExceptionMonitor", (err, origin) => {
-    //     console.log(err, origin)
-    //     const uncaughtExceptionMonitorEmbed = new MessageEmbed()
-    //         .setTitle('Uncaught Exception Monitor')
-    //         .setColor('RED')
-    //         .addField('Error', `\`\`\`${inspect(err, { depth: 0 }).substring(0, 1000)}\`\`\``)
-    //         .addField('Origin', `\`\`\`${inspect(origin, { depth: 0 }).substring(0, 1000)}\`\`\``)
-    //         .setTimestamp()
-    
-    //     return s.send({
-    //         embeds: [uncaughtExceptionMonitorEmbed]
-    //     })
-    // });
+    process.on("uncaughtExceptionMonitor", (err, origin) => {
+        console.log(err, origin)
+        const uncaughtExceptionMonitorEmbed = new MessageEmbed()
+            .setTitle('Uncaught Exception Monitor')
+            .setColor('RED')
+            .addField('Error', `\`\`\`${inspect(err, { depth: 0 }).substring(0, 1000)}\`\`\``)
+            .addField('Origin', `\`\`\`${inspect(origin, { depth: 0 }).substring(0, 1000)}\`\`\``)
+            .setTimestamp()
+        
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [uncaughtExceptionMonitorEmbed] })
+    });
     
     process.on("multipleResolves", (type, promise, reason) => {
         console.log(type, promise, reason)
@@ -70,9 +64,9 @@ module.exports = (client) => {
             .addField('Promise', `\`\`\`${inspect(promise, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .addField('Reason', `\`\`\`${inspect(reason, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .setTimestamp()
-        return s.send({
-            embeds: [multipleResolvesEmbed]
-        })
+
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [multipleResolvesEmbed] })
     });
     
     process.on("warning", (warn) => {
@@ -83,8 +77,8 @@ module.exports = (client) => {
             .setColor('RED')
             .addField('Warn', `\`\`\`${inspect(warn, { depth: 0 }).substring(0, 1000)}\`\`\``)
             .setTimestamp()
-        return s.send({
-            embeds: [warningEmbed]
-        })
+
+        const channel = client.channels.cache.get(error_logs_id)
+        return channel.send({ embeds: [warningEmbed] })
     });  
 }
