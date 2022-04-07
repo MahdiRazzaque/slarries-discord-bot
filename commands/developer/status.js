d2b82be1026d76db36b8ddd71df627ec6ad74d34
MahdiRazzaque <mahdi@razzaque.co.uk>
2022-04-07 16:51:25 +0100

    Update status.js

--- commands/developer/status.js
+++ commands/developer/status.js
@@ -1,6 +1,8 @@
-const { CommandInteraction, Client, MessageEmbed } = require("discord.js");
+const { CommandInteraction, Client, MessageEmbed, version } = require("discord.js");
 const { connection } = require("mongoose");
+const os = require("os")
 const { developer_embed_colour } = require("../../structures/config.json");
+
 
 module.exports = {
   name: "status",
@@ -14,35 +16,35 @@ module.exports = {
    * @param {CommandInteraction} interaction
    * @param {Client} client
    */
-  async execute(interaction, client) {      
+  async execute(interaction, client) {
+    await client.user.fetch();
+    await client.application.fetch();
+    
+    const status = [ "Disconnected", "Connected", "Connecting", "Disconnecting" ];
+    
     const Response = new MessageEmbed()
       .setColor(developer_embed_colour)
-      .setTitle("Status")
+      .setTitle(`🧙🏻‍♂️ ${client.user.username} Status`)
+      .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+      .setDescription(interaction.client.application.description || "")
       .addFields(
-        { name: "**Client** 🕵️", value: `🟢 Online` },
-        { name: "**Ping 🏓**", value: `${client.ws.ping}ms` },
-        { name: "**Uptime** 📈", value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>` },
-        { name: "**Database** 📚", value: `${switchTo(connection.readyState)}` }
+        { name: "🤖 Status", value: `🟢 Online`, inline: true},
+        { name: "🧠 Client", value: client.user.tag, inline: true },
+        { name: "📆 Created", value: `<t:${parseInt(client.user.createdTimestamp / 1000)}:R>`, inline: true },
+        { name: "☑ Verified", value: client.user.flags.has("VERIFIED_BOT") ? "Yes" : "No", inline: true },
+        { name: "👩🏻‍💻 Owner", value: `${interaction.client.application.owner.tag || "None"}`, inline: true },
+        { name: "📚 Database", value: status[connection.readyState], inline: true },
+        { name: "💾 Memory Usage", value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}%`, inline: true },
+        { name: "🖥 System", value: os.type().includes("Windows") ? "Windows" : os.type(), inline: true },
+        { name: "👩🏻‍🔧 Node.js", value: process.version, inline: true },
+        { name: "🛠 Discord.js", value: version, inline: true },
+        { name: "⏰ Up Since", value: `<t:${parseInt(client.readyTimestamp / 1000)}:R>`, inline: true },
+        { name: "🏓 Ping", value: `${client.ws.ping}ms`, inline: true },
+        { name: "🤹🏻‍♀️ Commands", value: `${client.commands.size}`, inline: true },
+        { name: "👨‍👩‍👧‍👦 Servers", value: `${client.guilds.cache.size}`, inline: true },
+        { name: "👧🏻 Users", value: `${client.users.cache.size}`, inline: true },
       )
+      
     interaction.reply({ embeds: [Response] });
   },
 };
-
-function switchTo(val) {
-  var status = " ";
-  switch (val) {
-    case 0:
-      status = `🔴 DISCONNECTED`;
-      break;
-    case 1:
-      status = `🟢 CONNECTED`;
-      break;
-    case 2:
-      status = `🟠 CONNECTING`;
-      break;
-    case 3:
-      status = `🟣 DISCONNECTING`;
-      break;
-  }
-  return status;
-}
